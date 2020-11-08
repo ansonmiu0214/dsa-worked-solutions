edb6a949cc95c6c689921a95ab6a949b701a281a
Anson Miu <me@ansonmiu.dev>
2020-11-08 01:34:58 +0000

    Fix typo

--- solutions/furthest_building/__main__.py
+++ solutions/furthest_building/__main__.py
@@ -6,5 +6,5 @@ heights = list(map(int, input().strip().split(' ')))
 print('Enter number of bricks and number of ladders, space-separated:', end=' ')
 [bricks, ladders] = list(map(int, input().strip().split(' ')))
 
-furthest = furthestBuilding(heights, bricks, laders)
+furthest = furthestBuilding(heights, bricks, ladders)
 print(f'Furthest reachable building: #{furthest}')
