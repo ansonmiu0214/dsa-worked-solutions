d646b07018013db1fef0a5593e49a1c93a497309
Anson Miu <me@ansonmiu.dev>
2020-11-20 22:08:00 +0000

    Use pluralised printer

--- solutions/minimum_deletions_balance_string/__main__.py
+++ solutions/minimum_deletions_balance_string/__main__.py
@@ -1,4 +1,5 @@
 from .solution import minimumDeletions
+from ..utils import printer
 
 print('Enter string only consisting of "a"s and "b"s:', end=' ')
 s = input().strip().lower()
@@ -7,4 +8,4 @@ violating_characters = set(s) - set('ab')
 assert len(violating_characters) == 0, f'Violating characters: {", ".join(violating_characters)}'
 
 deletions = minimumDeletions(s)
-print(f'Need at least {deletions} deletion(s) to make "{s}" balanced.')
+print(f'Need at least {printer.pluralise(deletion=deletions)} to make "{s}" balanced.')
