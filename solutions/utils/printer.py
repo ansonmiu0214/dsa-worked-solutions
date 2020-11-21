6cf8b8a0185f978ea233077ca4050035473a2dd3
Anson Miu <me@ansonmiu.dev>
2020-11-21 15:37:53 +0000

    Enhance pluralise function

--- solutions/utils/printer.py
+++ solutions/utils/printer.py
@@ -5,6 +5,11 @@ def pluralise(**kwargs) -> str:
     assert len(kwargs) == 1
 
     [(unit, val)] = kwargs.items()
-    suffix = 's' if not unit.endswith('s') else 'es'
+    if unit.endswith('s'):
+        transform = lambda s: s
+    elif unit.endswith('y'):
+        transform = lambda s: f'{s[:-1]}ies'
+    else:
+        transform = lambda s: f'{s}s'
 
-    return f'{val} {unit}{suffix if val != 1 else ""}'
+    return f'{val} {transform(unit)}'
